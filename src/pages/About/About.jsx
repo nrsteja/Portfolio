fb36370b99f1bb81916e2ca425f8e38298f70bb1
teja <tejeswara.rajani@gmail.com>
2024-06-29 20:11:46 +0800

    minor changes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,10 +3,6 @@ import "./About.css"
 import Buttons from '../../assets/components/Buttons/Buttons'
 import Back from '../../assets/components/Back/back'
 import { useNavigate } from 'react-router-dom'
-import MainCard from '../../assets/components/MainCard/MainCard.jsx';
-import AboutCard from '../../assets/components/AboutCard/AboutCard.jsx';
-import { experiencesData, projectsData } from '../../assets/data/database.jsx'
-import HelperPrompt from '../../assets/components/HelperPrompt/HelperPrompt.jsx'
 
 export default function About() {
     const navigate = useNavigate();
